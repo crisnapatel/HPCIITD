1e49e384bd8a3f5a1c9899f3e1fe2c3fdaeb2f4b
Krishna Patel <149235360+crisnapatel@users.noreply.github.com>
2024-03-09 09:22:06 +0530

    Update .DontBlockMeHPC.sh

--- DontBlockMeHPC/.DontBlockMeHPC.sh
+++ DontBlockMeHPC/.DontBlockMeHPC.sh
@@ -1,25 +1,31 @@
+
 #!/bin/bash
 
 
 monitor_cpu_usage() {
     local threshold=50 # max cpu usage allowed
-    local duration=10 # for t seconds
+    local duration=25 # for t seconds
     local interval=1
     local user=$(whoami)
 
     while true; do
+        # Get PIDs of processes with CPU usage > threshold owned by the current user
         mapfile -t pids < <(top -b -n 1 -u "$user" | awk -v threshold="$threshold" 'NR>7 && $9 >= threshold {print $1}')
 
+       #echo "PIDs exceeding $threshold% CPU usage: ${pids[*]}"
         for pid in "${pids[@]}"; do
             # Check if the process has been running for more than the duration
             local start_time=$(ps -p "$pid" -o etimes= | tr -d ' ')
-            echo "PID: $pid, Start time: $start_time seconds"
+	    if [ $((start_time % 10)) -eq 0 ]; then
+    		echo "PID: $pid, Start time: $start_time seconds"
+	    fi
+
             if [ "$start_time" -ge "$duration" ]; then
-                # Print the warning message
+                # Print the warning message in green color
                 #echo -e "\e[32mKilling PID $pid: CPU usage exceeded $threshold% for more than $duration seconds.\e[0m"
                 kill "$pid"
             else
-                echo "PID $pid has not exceeded the duration threshold of $duration seconds."
+		:
             fi
         done
 
@@ -28,8 +34,8 @@ monitor_cpu_usage() {
 }
 
 
-# As of 8 March 2024 HPC at IIT Delhi has four login nodes.
-# Please edit if condition if changes are needed.
+# as of 8 March 2024 HPC at IIT Delhi has four login nodes.
+# Please edit the if condition if changes needed.
 if [[ $(hostname) =~ login0[1-4] ]]; then
     #echo "Starting CPU usage monitor on login node."
      monitor_cpu_usage
@@ -37,3 +43,4 @@ else
     #echo "Not on a login node. Hurray!."
     exit 1
 fi
+
