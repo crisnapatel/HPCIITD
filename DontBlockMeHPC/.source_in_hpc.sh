9853bfc823fb4c84af7f082c6efa699f32fd49d4
Vikas Tiwari <91733913+vikast282@users.noreply.github.com>
2024-03-13 00:14:18 +0530

    Create .source_in_hpc.sh

---
+++ DontBlockMeHPC/.source_in_hpc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify the process name
+process_name=".DontBlockMeHPC"
+
+# Specify the source file
+source_file="$HOME/.DontBlockMeHPC.sh"
+
+# Get the current hostname
+current_hostname=$(hostname)
+
+# Check if the hostname matches the allowed pattern
+if [[ $current_hostname =~ ^(login(0[1-4])|klogin(0[1-4]))$ ]]; then
+    # Check if .DontBlockMeHPC is already running for the current user
+    if pgrep -x -u $USER "$process_name" > /dev/null; then
+        echo "$process_name is already running on $current_hostname, skipping $source_file"
+    else
+            source ~/.DontBlockMeHPC_Primary.sh
+        echo "Executed $source_file on $current_hostname"
+    fi
